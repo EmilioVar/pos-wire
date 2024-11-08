d6c6009742a6483d00a76ba69d3ea99ed8890167
Emilio Vargas Millan <emiliovm@outlook.es>
2024-11-08 20:46:41 +0100

    feat: límite de mesas

--- app/Livewire/Tables/Create.php
+++ app/Livewire/Tables/Create.php
@@ -8,11 +8,17 @@ use Livewire\Component;
 class Create extends Component
 {
     public function createTable() {
-        $tables = Table::latest()->first();
-        
-        Table::create();
+        $tables = Table::all();
 
-        $this->dispatch('createdTable');
+        if (count($tables) > 10) {
+            $tables = Table::latest()->first();
+            
+            Table::create();
+    
+            $this->dispatch('createdTable');
+        } else {
+            $this->dispatch('alert', type: "error", text: 'no se pueden crear más mesas');
+        }
     }
     public function render()
     {
